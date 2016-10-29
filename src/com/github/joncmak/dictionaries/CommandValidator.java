153e831966d2ba7332b363f6454a69679b1e636e
joncmak <joncmak@gmail.com>
2016-10-28 19:08:32 -0700

    added dictionaries, command validators, and special strings

---
+++ src/com/github/joncmak/dictionaries/CommandValidator.java
@@ -0,0 +1,9 @@
+package com.github.joncmak.dictionaries;
+
+public class CommandValidator
+{
+	public static final String[] VALID_ACTIONS = {"move", "use", "item", "quit"};
+	public static final String[] VALID_DIRECTIONS_SHORT = {"N", "E", "S", "W"};
+	public static final String[] VALID_DIRECTIONS_FULL = {"North", "East", "South", "West"};
+	public static final String[] VALID_DIRECTIONS_ALT = {"Up", "Right", "Down", "Left"};
+}
---
+++ src/com/github/joncmak/dictionaries/SpecialStrings.java
@@ -0,0 +1,6 @@
+package com.github.joncmak.dictionaries;
+
+public class SpecialStrings
+{
+	public static final String CLEAR_CONSOLE = "\033[H\033[2J";
+}
